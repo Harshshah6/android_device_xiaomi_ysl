ab9b2c89109a3578d6908db136f653354dc9fc13
Harsh Tagra <74857033+Harsh-Tagra@users.noreply.github.com>
2024-07-18 10:56:34 -0400

    set TARGET_SUPPORTS_64_BIT_APPS:= true

--- lineage_ysl.mk
+++ lineage_ysl.mk
@@ -15,8 +15,11 @@ $(call inherit-product, device/xiaomi/ysl/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 # Flags
 TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_64_BIT_APPS:= true
+
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
 PRODUCT_NAME := lineage_ysl
