7b064d9b2dc6b0d8d7281d50151c25c89faf98e5
Harsh-Tagra <harshtagra905@gmail.com>
2024-08-07 10:16:49 -0400

    evo init

--- lineage_ysl.mk
+++ lineage_ysl.mk
@@ -16,6 +16,11 @@ $(call inherit-product, device/xiaomi/ysl/device.mk)
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Flags
+EVO_BUILD_TYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_SUPPORTS_64_BIT_APPS:= true
+TARGET_USES_MINI_GAPPS := true
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ysl
 PRODUCT_NAME := lineage_ysl
